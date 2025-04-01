0276ad8954c14cda17f7bc70fd2af05deca2f5f5
nightspringer <luvbmjn@gmail.com>
2025-04-01 02:41:52 -0400

    Update config.rb

--- lib/retrospring/config.rb
+++ lib/retrospring/config.rb
@@ -4,28 +4,31 @@ module Retrospring
   module Config
     module_function
 
-    def config_hash = {}.with_indifferent_access.tap do |hash|
-      # load yml config if it's present
-      justask_yml_path = Rails.root.join("config/justask.yml")
-      hash.merge!(YAML.load_file(justask_yml_path)) if File.exist?(justask_yml_path)
+    def config_hash
+      {}.with_indifferent_access.tap do |hash|
+        # Load local YAML config (justask.yml renamed for NightSpring)
+        yml_path = Rails.root.join("config/justask.yml")
+        hash.merge!(YAML.load_file(yml_path)) if File.exist?(yml_path)
 
-      # load config from ENV where possible
-      env_config = {
-        # The site name, shown everywhere
-        site_name: ENV.fetch("SITE_NAME", nil),
-      }.compact
-      hash.merge!(env_config)
+        # Optional overrides from ENV
+        env_config = {
+          site_name: ENV["SITE_NAME"],
+          hostname: ENV["HOSTNAME"],
+          https: ENV["HTTPS"]&.downcase == "true"
+        }.compact
 
-      # Update rails config for mail
-      Rails.application.config.action_mailer.default_url_options = {
-        host: hash["hostname"],
-      }
+        hash.merge!(env_config)
+
+        # Update default mail host for Devise and others
+        Rails.application.config.action_mailer.default_url_options = {
+          host: hash["hostname"],
+          protocol: hash["https"] ? "https" : "http"
+        }
+      end
     end
 
     def registrations_enabled? = feature_enabled?(:registration)
-
     def advanced_frontpage_enabled? = feature_enabled?(:advanced_frontpage)
-
     def readonly? = feature_enabled?(:readonly)
 
     def feature_enabled?(feature) = APP_CONFIG.dig(:features, feature, :enabled)
