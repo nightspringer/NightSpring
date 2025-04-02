9e563cb8874d363d9a8a02a302ae5d3fedb58ea6
nightspringer <luvbmjn@gmail.com>
2025-04-02 13:38:15 -0400

    Update retromail.rb

--- app/mailers/retromail.rb
+++ app/mailers/retromail.rb
@@ -1,10 +1,12 @@
 class Retromail < Devise::Mailer
   helper :application
   include Devise::Controllers::UrlHelpers
-  default template_path: 'devise/mailer', parts_order: [ 'text/plain', 'text/html' ]
+  default from: "#{ENV.fetch('APP_SITE_NAME', 'NightSpring')} <#{ENV.fetch('APP_EMAIL_FROM', 'noreply@nightspring.net')}>",
+          template_path: 'devise/mailer',
+          parts_order: ['text/plain', 'text/html']
   layout 'mail'
 
-  def devise_mail(record, action, opts={})
+  def devise_mail(record, action, opts = {})
     initialize_from_record(record)
     mail(headers_for(action, opts)) do |format|
       format.text
@@ -12,7 +14,7 @@ class Retromail < Devise::Mailer
     end
   end
 
-  def confirmation_instructions(record, token, opts={})
+  def confirmation_instructions(record, token, opts = {})
     @token = token
     devise_mail(record, :confirmation_instructions, opts)
   end
